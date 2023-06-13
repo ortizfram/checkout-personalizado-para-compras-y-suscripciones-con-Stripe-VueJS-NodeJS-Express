1917c7f4c0cc5b1e52fbf46544b73073eb6b7ac2
ortizfram <ortizfranco48@gmail.com>
2023-06-13 12:29:43 -0300

    get subscriptions ProfileController

--- api/controllers/ProfileController.js
+++ api/controllers/ProfileController.js
@@ -1,8 +1,26 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 class ProfileController {
-  indexSubscriptions(req, res) {
-    res.send({
-      a: "asdasd",
+  async indexSubscriptions(req, res) {
+    const customerId = req.params.customer_id;
+
+    const subscriptions = await stripe.subscriptions.list({
+      customer: customerId,
+      status: "all",
+      expand: ["data.default_payment_method"],
     });
+
+    if (subscriptions) {
+      res.send({
+        status: true,
+        data: subscriptions.data,
+      });
+    } else {
+      res.send({
+        status: false,
+        message: "No subscriptions found",
+      });
+    }
   }
 }
 
